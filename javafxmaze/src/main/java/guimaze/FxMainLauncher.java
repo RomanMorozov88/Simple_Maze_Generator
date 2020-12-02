1a46745618590f655687d83fc02e1689b9cf3d04
RomanMorozov88 <MorozovRoman.88@mail.ru>
2020-12-02 09:02:36 +0300

    fx-maze executable jar finally

---
+++ javafxmaze/src/main/java/guimaze/FxMainLauncher.java
@@ -0,0 +1,11 @@
+package guimaze;
+
+/**
+ * Для создания запускаемого .jar- см. видео:
+ * https://www.youtube.com/watch?v=w_PsCNqqcE4&t
+ */
+public class FxMainLauncher {
+    public static void main(String[] args) {
+        FxMainClass.main(args);
+    }
+}
